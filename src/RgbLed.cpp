e8edf1c023f213833eb430bbb196f91c48fc5d18
odeded <odedsh@yahoo.com>
2019-05-28 21:49:28 +0300

    Add REG led

--- src/RgbLed.cpp
+++ src/RgbLed.cpp
@@ -2,16 +2,16 @@
 
 #include "Arduino.h"
 
-RgbLed::map_type RgbLed::colorValues = {
-    {RgbLed::color::WHITE, {255, 255, 255}},
-    {RgbLed::color::BLACK, {0, 0, 0}},
-    {RgbLed::color::RED, {255, 0, 0}},
-    {RgbLed::color::GREEN, {0, 255, 0}},
-    {RgbLed::color::BLUE, {0, 0, 255}},
-    {RgbLed::color::GRAY, {128, 128, 128}},
-    {RgbLed::color::YELLOW, {255, 255, 0}},
-    {RgbLed::color::CYAN, {0, 255, 255}},
-    {RgbLed::color::MAGENTA, {255, 0, 255}}
+const RgbLed::map_type RgbLed::colorValues = {
+    {RgbLed::color::WHITE, tuple_type(255, 255, 255)},
+    {RgbLed::color::BLACK, tuple_type(0, 0, 0)},
+    {RgbLed::color::RED, tuple_type(255, 0, 0)},
+    {RgbLed::color::GREEN, tuple_type(0, 255, 0)},
+    {RgbLed::color::BLUE, tuple_type(0, 0, 255)},
+    {RgbLed::color::GRAY, tuple_type(128, 128, 128)},
+    {RgbLed::color::YELLOW, tuple_type(255, 255, 0)},
+    {RgbLed::color::CYAN, tuple_type(0, 255, 255)},
+    {RgbLed::color::MAGENTA, tuple_type(255, 0, 255)}
 };
 
 RgbLed::RgbLed(int rPin, int gPin, int bPin)
@@ -24,11 +24,12 @@ RgbLed::RgbLed(int rPin, int gPin, int bPin)
     ledcAttachPin(greenPin, 2);
     ledcAttachPin(bluePin, 3);
 
-    ledcSetup(1, 12000, 8);
-    ledcSetup(2, 12000, 8);
-    ledcSetup(3, 12000, 8);
+    int frequency = 500;
+    ledcSetup(1, frequency, 8);
+    ledcSetup(2, frequency, 8);
+    ledcSetup(3, frequency, 8);
 
-    setColor(0, 250, 0);
+    setColor(color::BLACK);
 }
 
 void RgbLed::setColor(int red, int green, int blue)
--- src/RgbLed.h
+++ src/RgbLed.h
@@ -20,14 +20,14 @@ public:
         CYAN,
         MAGENTA
     };
-
+    
     void setColor(int red, int green, int blue);
     void setColor(RgbLed::color _color);
-    
-    using tuple_type = std::tuple<int, int, int>;
-    using map_type = std::map<RgbLed::color, tuple_type>;
 
-    static map_type colorValues;
+    using tuple_type = std::tuple<int, int, int>;
+    using map_type = std::map<RgbLed::color, RgbLed::tuple_type>;
+    static const map_type colorValues;
+
 private:
     //LED Pins
 	int redPin;
